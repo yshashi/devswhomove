ae3f8be40557176c75622cfb973e4881cf3a045e
Santosh Yadav <santosh.yadav198613@gmail.com>
2025-09-01 22:25:05 +0200

    add nestjs api project

---
+++ backend/devswhorun-api/jest.config.ts
@@ -0,0 +1,10 @@
+export default {
+  displayName: 'devswhorun-api',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/backend/devswhorun-api',
+};
